db455075496c5044f77b89c94389168dd4b6627f
koushik <koushik.dey@live.com>
2022-08-09 00:32:17 +0300

    location1

---
+++ location/script.js
@@ -0,0 +1,9 @@
+const display = document.getElementById("display");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        display.innerHTML("Geolocation is not available in your browser!")
+    }
+}
